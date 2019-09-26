0bf3f8d074ced46741beea12363357f732f04e46
Margarita <ricunsos@gmail.com>
2019-09-26 21:34:47 +0300

    убрать лишнее

--- lab3-1/lab3-1/Form1.Designer.cs
+++ lab3-1/lab3-1/Form1.Designer.cs
@@ -44,9 +44,10 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(622, 414);
+            this.pictureBox1.Size = new System.Drawing.Size(829, 510);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -56,10 +57,10 @@
             // 
             // chooseColorBtn
             // 
-            this.chooseColorBtn.Location = new System.Drawing.Point(6, 28);
-            this.chooseColorBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.chooseColorBtn.Location = new System.Drawing.Point(8, 34);
+            this.chooseColorBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chooseColorBtn.Name = "chooseColorBtn";
-            this.chooseColorBtn.Size = new System.Drawing.Size(144, 26);
+            this.chooseColorBtn.Size = new System.Drawing.Size(192, 32);
             this.chooseColorBtn.TabIndex = 1;
             this.chooseColorBtn.Text = "Цвет заливки";
             this.chooseColorBtn.UseVisualStyleBackColor = true;
@@ -67,10 +68,10 @@
             // 
             // chooseImageBtn
             // 
-            this.chooseImageBtn.Location = new System.Drawing.Point(6, 74);
-            this.chooseImageBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.chooseImageBtn.Location = new System.Drawing.Point(8, 91);
+            this.chooseImageBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chooseImageBtn.Name = "chooseImageBtn";
-            this.chooseImageBtn.Size = new System.Drawing.Size(143, 26);
+            this.chooseImageBtn.Size = new System.Drawing.Size(191, 32);
             this.chooseImageBtn.TabIndex = 2;
             this.chooseImageBtn.Text = "Картинка";
             this.chooseImageBtn.UseVisualStyleBackColor = true;
@@ -79,9 +80,10 @@
             // radioPen
             // 
             this.radioPen.AutoSize = true;
-            this.radioPen.Location = new System.Drawing.Point(6, 19);
+            this.radioPen.Location = new System.Drawing.Point(8, 23);
+            this.radioPen.Margin = new System.Windows.Forms.Padding(4);
             this.radioPen.Name = "radioPen";
-            this.radioPen.Size = new System.Drawing.Size(76, 17);
+            this.radioPen.Size = new System.Drawing.Size(97, 21);
             this.radioPen.TabIndex = 8;
             this.radioPen.Text = "Карандаш";
             this.radioPen.UseVisualStyleBackColor = true;
@@ -89,9 +91,10 @@
             // radioFillColor
             // 
             this.radioFillColor.AutoSize = true;
-            this.radioFillColor.Location = new System.Drawing.Point(6, 42);
+            this.radioFillColor.Location = new System.Drawing.Point(8, 52);
+            this.radioFillColor.Margin = new System.Windows.Forms.Padding(4);
             this.radioFillColor.Name = "radioFillColor";
-            this.radioFillColor.Size = new System.Drawing.Size(108, 17);
+            this.radioFillColor.Size = new System.Drawing.Size(135, 21);
             this.radioFillColor.TabIndex = 9;
             this.radioFillColor.Text = "Заливка цветом";
             this.radioFillColor.UseVisualStyleBackColor = true;
@@ -99,9 +102,10 @@
             // radioFillTexture
             // 
             this.radioFillTexture.AutoSize = true;
-            this.radioFillTexture.Location = new System.Drawing.Point(6, 65);
+            this.radioFillTexture.Location = new System.Drawing.Point(8, 80);
+            this.radioFillTexture.Margin = new System.Windows.Forms.Padding(4);
             this.radioFillTexture.Name = "radioFillTexture";
-            this.radioFillTexture.Size = new System.Drawing.Size(124, 17);
+            this.radioFillTexture.Size = new System.Drawing.Size(157, 21);
             this.radioFillTexture.TabIndex = 10;
             this.radioFillTexture.Text = "Заливка картинкой";
             this.radioFillTexture.UseVisualStyleBackColor = true;
@@ -111,9 +115,11 @@
             this.groupBox1.Controls.Add(this.radioPen);
             this.groupBox1.Controls.Add(this.radioFillColor);
             this.groupBox1.Controls.Add(this.radioFillTexture);
-            this.groupBox1.Location = new System.Drawing.Point(640, 12);
+            this.groupBox1.Location = new System.Drawing.Point(853, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(154, 101);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(205, 123);
             this.groupBox1.TabIndex = 12;
             this.groupBox1.TabStop = false;
             // 
@@ -121,18 +127,21 @@
             // 
             this.groupBox2.Controls.Add(this.chooseColorBtn);
             this.groupBox2.Controls.Add(this.chooseImageBtn);
-            this.groupBox2.Location = new System.Drawing.Point(640, 136);
+            this.groupBox2.Location = new System.Drawing.Point(853, 181);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(154, 118);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(205, 145);
             this.groupBox2.TabIndex = 13;
             this.groupBox2.TabStop = false;
             // 
             // clearButton
             // 
             this.clearButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.clearButton.Location = new System.Drawing.Point(640, 295);
+            this.clearButton.Location = new System.Drawing.Point(853, 402);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(4);
             this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(154, 45);
+            this.clearButton.Size = new System.Drawing.Size(205, 55);
             this.clearButton.TabIndex = 16;
             this.clearButton.Text = "Очистить";
             this.clearButton.UseVisualStyleBackColor = true;
@@ -140,14 +149,15 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(801, 433);
+            this.ClientSize = new System.Drawing.Size(1068, 533);
             this.Controls.Add(this.clearButton);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
